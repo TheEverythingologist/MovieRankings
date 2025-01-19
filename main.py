643d94ac201921a3d5516eecd8d6eb7837a12d58
Conner White <tkd12white@gmail.com>
2025-01-19 16:26:02 -0600

    main bare bones and interface

---
+++ main.py
@@ -0,0 +1,10 @@
+from movie import Movie
+from rankingalgorithm import RankingSystem
+from reader import Reader
+from writer import Writer
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()

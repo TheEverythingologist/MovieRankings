33184f2376ebd5063ededa114f7a871402cb7bd6
Conner White <tkd12white@gmail.com>
2025-01-19 16:16:56 -0600

    write to csv

--- writer.py
+++ writer.py
@@ -1,3 +1,8 @@
+import pandas as pd
+
 class Writer:
     def __init__(self):
         pass
+
+    def write_to_csv(self, dataframe: pd.DataFrame, file_name:str):
+        dataframe.to_csv(file_name, sep=", ", encoding='utf-8')

1d99411da4dd73b6dd2cee897f26d0fc6e700c8a
Conner White <tkd12white@gmail.com>
2025-01-19 13:58:26 -0600

    planned functionality for Movie

--- movie.py
+++ movie.py
@@ -5,3 +5,23 @@ class Movie:
         self.name = name
         self.release_year = release_year
         self.initial_score = MIDPOINT_SCORE
+
+    #TODO Add ability to get rankings from various websites like Letterboxd, IMDB, Rotten Tomatoes, etc.
+
+    def getRottenTomatoesLink(self):
+        pass
+
+    def getRottenTomatoesScore(self):
+        pass
+
+    def getIMDBLink(self):
+        pass
+
+    def getIMDBScore(self):
+        pass
+
+    def getLetterboxdLink(self):
+        pass
+
+    def getLetterboxdScore(self):
+        pass

a18a2d975839c76e491311d08b9df84d9eb0c6de
Conner White <tkd12white@gmail.com>
2025-01-19 18:06:29 -0600

    edits to writer

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from movie import Movie
 from rankingalgorithm import RankingSystem
 from moviereader import MovieReader
-from writer import Writer
+from writer import MovieWriter
+from interface import Interface
 
 def main():
-    pass
-    # read in movies list
-    # add new movies to the database
+    # Read in movies list
+    path_to_list_of_movies = "C:/Users/TKD12/OneDrive/Desktop/CodingRepos/MovieRanking/data/list_of_movies.txt"
+    movie_reader = MovieReader()
+    new_movies_list = movie_reader.read_listofmovies(file_path=path_to_list_of_movies)
+
+    # Add new movies to the database
+    path_to_database = "C:/Users/TKD12/OneDrive/Desktop/CodingRepos/MovieRanking/data/database.csv"
+    movie_writer = MovieWriter()
+    new_movie_df = movie_writer.generate_new_movie_dataframe(list_of_new_movies=new_movies_list)
+    movie_writer.write_to_csv(dataframe=new_movie_df, file_name=path_to_database)
+
     # Begin loop
-    # draw two movies from the database, prioritizing ones with fewer competitions
-    # have the two movies compete
-    # calculate the new elo of the two movies
-    # update the database
+    # Draw two movies from the database, prioritizing ones with fewer competitions
+    # Have the two movies compete
+    # Calculate the new elo of the two movies
+    # Update the database
     # Repeat Loop 
 
 
--- writer.py
+++ writer.py
@@ -1,11 +1,12 @@
 import pandas as pd
+from movie import Movie
 
-class Writer:
+class MovieWriter:
     def __init__(self):
         pass
 
     def write_to_csv(self, dataframe: pd.DataFrame, file_name:str):
-        dataframe.to_csv(file_name, sep=", ", encoding='utf-8')
+        dataframe.to_csv(file_name, sep=",", encoding='utf-8')
 
     def add_to_csv(self, dataframe: pd.DataFrame, file_name:str):
         dataframe.to_csv(file_name, mode='a', header=False)
@@ -14,4 +15,10 @@ class Writer:
         _df = pd.DataFrame(list_of_new_movies, columns=["Movie Name", "Release Year"])
         _df["Elo Rating"] = 2000
         _df["Times Competed"] = 0
+        _df.insert(0, "Rank", range)
         return _df
+    
+    def update_database(self, movie1:Movie, movie2:Movie):
+        # Update the elo ranking of both movies
+        # Update the rank column
+        pass

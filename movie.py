73f8a45bc6ae5ca786f0a0ea66d63f518bb09a0c
Conner White <tkd12white@gmail.com>
2025-08-04 22:47:48 -0500

    venv

--- movie.py
+++ movie.py
@@ -1,21 +1,25 @@
-MIDPOINT_SCORE = 2000
+import re
+
+MIDPOINT_SCORE = 1000
 
 class Movie:
     def __init__(self, name, release_year):
         self.name = name
         self.release_year = release_year
         self.initial_score = MIDPOINT_SCORE
+        self.letterboxd_link = self.getLetterboxdLink()
 
     #TODO Add ability to get rankings from various websites like Letterboxd, IMDB, Rotten Tomatoes, etc.
 
-    def getIMDBLink(self):
-        pass
+    # def getIMDBLink(self):
+    #     pass
 
-    def getIMDBScore(self):
-        pass
+    # def getIMDBScore(self):
+    #     pass
 
     def getLetterboxdLink(self):
-        pass
+        name_in_link = slugify(self.name)
+        link = f"https://letterboxd.com/film/{name_in_link}/"
 
     def getLetterboxdScore(self):
         pass
@@ -28,3 +32,12 @@ class Movie:
             return True
         else:
             return False
+        
+
+def slugify(text: str) -> str:
+    # Replace non-alphanumeric characters with dashes
+    text = re.sub(r'[^a-zA-Z0-9]', '-', text)
+    # Replace multiple dashes with a single dash
+    text = re.sub(r'-+', '-', text)
+    # Strip leading/trailing dashes and lowercase
+    return text.strip('-').lower()
